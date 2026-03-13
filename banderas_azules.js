0ed70b60517694ec5130203d360490d50cb8f425
AnaBMo <anab_mj@outlook.com>
2026-03-13 12:28:12 +0100

    Add playas bandera azul 2025

---
+++ banderas_azules.js
@@ -0,0 +1,259 @@
+// Banderas Azules 2025 - Playas a 40km de Elche
+const banderasAzules = [
+    {
+        nombre: 'Postiguet',
+        municipio: 'Alicante/Alacant',
+        lat: 38.347077,
+        lng: -0.475456,
+        distancia: 21.38,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Saladar-Urbanova',
+        municipio: 'Alicante/Alacant',
+        lat: 38.278492,
+        lng: -0.519755,
+        distancia: 15.76,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Sant Joan',
+        municipio: 'Alicante/Alacant',
+        lat: 38.37503,
+        lng: -0.409317,
+        distancia: 27.92,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Tabarca',
+        municipio: 'Alicante/Alacant',
+        lat: 38.165829,
+        lng: -0.478931,
+        distancia: 22.52,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Carrer de la Mar',
+        municipio: 'Campello (el)',
+        lat: 38.427405,
+        lng: -0.389184,
+        distancia: 32.26,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Mutxavista',
+        municipio: 'Campello (el)',
+        lat: 38.401841,
+        lng: -0.404842,
+        distancia: 29.63,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Arenals del Sol-Sur',
+        municipio: 'Elche/Elx',
+        lat: 38.247537,
+        lng: -0.515661,
+        distancia: 16.29,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Carabassí',
+        municipio: 'Elche/Elx',
+        lat: 38.224855,
+        lng: -0.510636,
+        distancia: 17.28,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'L’Altet',
+        municipio: 'Elche/Elx',
+        lat: 38.239184,
+        lng: -0.514412,
+        distancia: 16.56,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'La Marina',
+        municipio: 'Elche/Elx',
+        lat: 38.140351,
+        lng: -0.635298,
+        distancia: 15.49,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Les Pesqueres-El Rebollo',
+        municipio: 'Elche/Elx',
+        lat: 38.129095,
+        lng: -0.638856,
+        distancia: 16.55,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Centre',
+        municipio: 'Guardamar del Segura',
+        lat: 38.080588,
+        lng: -0.651887,
+        distancia: 21.48,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Ortigues-Campo NUEVA',
+        municipio: 'Guardamar del Segura',
+        lat: 38.038959,
+        lng: -0.651728,
+        distancia: 26.04,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Del Moncaio',
+        municipio: 'Guardamar del Segura',
+        lat: 38.066184,
+        lng: -0.648903,
+        distancia: 23.1,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'La Roqueta',
+        municipio: 'Guardamar del Segura',
+        lat: 38.075678,
+        lng: -0.647695,
+        distancia: 22.09,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'De Vivers',
+        municipio: 'Guardamar del Segura',
+        lat: 38.097519,
+        lng: -0.645201,
+        distancia: 19.77,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Cabo Roig -La Caleta',
+        municipio: 'Orihuela',
+        lat: 37.911891,
+        lng: -0.733159,
+        distancia: 39.93,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Cala Capitán',
+        municipio: 'Orihuela',
+        lat: 37.92042,
+        lng: -0.722162,
+        distancia: 38.92,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Cala Cerrada',
+        municipio: 'Orihuela',
+        lat: 37.926738,
+        lng: -0.721358,
+        distancia: 38.21,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Cala Estaca',
+        municipio: 'Orihuela',
+        lat: 37.928921,
+        lng: -0.720483,
+        distancia: 37.97,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'La Zenia- Cala Bosque',
+        municipio: 'Orihuela',
+        lat: 37.925719,
+        lng: -0.72123,
+        distancia: 38.33,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Punta Prima',
+        municipio: 'Orihuela',
+        lat: 37.943757,
+        lng: -0.709,
+        distancia: 36.29,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Calas del Este',
+        municipio: 'Santa Pola',
+        lat: 38.192835,
+        lng: -0.524602,
+        distancia: 17.56,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Calas de Santiago Bernabéu',
+        municipio: 'Santa Pola',
+        lat: 38.18936,
+        lng: -0.548286,
+        distancia: 16.0,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'La Ermita',
+        municipio: 'Santa Pola',
+        lat: 38.225405,
+        lng: -0.513207,
+        distancia: 17.05,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Varador',
+        municipio: 'Santa Pola',
+        lat: 38.190604,
+        lng: -0.535671,
+        distancia: 16.85,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Cabo Cervera',
+        municipio: 'Torrevieja',
+        lat: 38.005771,
+        lng: -0.648905,
+        distancia: 29.72,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Cala de las Piteras',
+        municipio: 'Torrevieja',
+        lat: 37.946231,
+        lng: -0.705507,
+        distancia: 36.0,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'El Cura',
+        municipio: 'Torrevieja',
+        lat: 37.97902,
+        lng: -0.667532,
+        distancia: 32.48,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'La Mata (antigua La Mata Sur)',
+        municipio: 'Torrevieja',
+        lat: 38.021828,
+        lng: -0.652779,
+        distancia: 27.9,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Los Locos',
+        municipio: 'Torrevieja',
+        lat: 37.984827,
+        lng: -0.659685,
+        distancia: 31.91,
+        tipo: 'playa'
+    },
+    {
+        nombre: 'Los Náufragos',
+        municipio: 'Torrevieja',
+        lat: 37.970388,
+        lng: -0.69549,
+        distancia: 33.32,
+        tipo: 'playa'
+    },
+];
