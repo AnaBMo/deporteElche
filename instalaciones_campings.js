bd3b93260d58b6ee71b51f8dd5a7dc6678ee5c4a
AnaBMo <anab_mj@outlook.com>
2026-03-15 18:44:16 +0100

    Add campings

---
+++ instalaciones_campings.js
@@ -0,0 +1,407 @@
+var instalacionesCampings = [
+    {
+        nombre: 'Finca Oasis',
+        direccion: 'Partida, 24, Crevillent',
+        categoria: 'camping',
+        rating: 5.0,
+        lat: 38.1978404,
+        lng: -0.826807,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Crevillente',
+        direccion: 'Parcela HA10, Partida Las Casicas, 5, Catral',
+        categoria: 'camping',
+        rating: 5.0,
+        lat: 38.1802285,
+        lng: -0.8146346,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'El Terrenito',
+        direccion: 'Elx',
+        categoria: 'area_autocaravanas',
+        rating: 5.0,
+        lat: 38.2034489,
+        lng: -0.6427704,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Casa Huerta',
+        direccion: 'Poligono 3 nr.55',
+        categoria: 'area_autocaravanas',
+        rating: 5.0,
+        lat: 38.1828712,
+        lng: -0.6752946999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camperplaats Levantri',
+        direccion: 'Diseminado Cebadas, 182A',
+        categoria: 'camping',
+        rating: 4.9,
+        lat: 38.14211359999999,
+        lng: -0.7812644,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Albatera Mobile Home Park',
+        direccion: 'Calle de Dr. Angel Diez Gascon, 3, Albatera',
+        categoria: 'area_autocaravanas',
+        rating: 4.9,
+        lat: 38.18752130000001,
+        lng: -0.895367,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Area 15, Camper',
+        direccion: 'C. Castilla y León, 1, Formentera del Segura',
+        categoria: 'camping',
+        rating: 4.8,
+        lat: 38.0865078,
+        lng: -0.7434341,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Casa Rosada',
+        direccion: 'Partida Hoya, poligono 3 nr 69, La Hoya',
+        categoria: 'area_autocaravanas',
+        rating: 4.8,
+        lat: 38.1813319,
+        lng: -0.6764690999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Casa La Bella Vida',
+        direccion: 'Cam. Lo Vera, 7',
+        categoria: 'area_autocaravanas',
+        rating: 4.8,
+        lat: 38.142285,
+        lng: -0.7911987,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camper Park Alicante',
+        direccion: 'Carrer de Juan de la Cierva, 8, El Campello',
+        categoria: 'camping',
+        rating: 4.8,
+        lat: 38.394854,
+        lng: -0.4096565,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Zona de acampada de Orito',
+        direccion: 'Monforte del Cid',
+        categoria: 'camping',
+        rating: 4.8,
+        lat: 38.3729904,
+        lng: -0.6726274999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Sheppards Rest',
+        direccion: 'Vereda Los Ñíguez, 99, Dolores',
+        categoria: 'camping',
+        rating: 4.8,
+        lat: 38.1562049,
+        lng: -0.7548541999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'De Wevers',
+        direccion: 'Partida veintiuno, nr 14, S. Isidro',
+        categoria: 'area_autocaravanas',
+        rating: 4.8,
+        lat: 38.1919191,
+        lng: -0.8349898,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Finca 010',
+        direccion: 'Partida Daimes, 44, poligono 2, Elx',
+        categoria: 'camping',
+        rating: 4.8,
+        lat: 38.1950583,
+        lng: -0.6977398,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Casa Castor Catral',
+        direccion: 'Partida El Realengo, 38, El Realengo',
+        categoria: 'area_autocaravanas',
+        rating: 4.8,
+        lat: 38.1886715,
+        lng: -0.8270291,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Kampaoh Crevillent',
+        direccion: 'salida, 730, AP-7, Crevillent',
+        categoria: 'camping',
+        rating: 4.7,
+        lat: 38.1806806,
+        lng: -0.8118788,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'El Paraisopark - Rental MOBILE HOMES Plots',
+        direccion: 'Av. del Calvario, Albatera',
+        categoria: 'camping',
+        rating: 4.7,
+        lat: 38.187304,
+        lng: -0.8823761999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Casa Ronach',
+        direccion: 'Cam. lo Chicharra, 74B',
+        categoria: 'area_autocaravanas',
+        rating: 4.7,
+        lat: 38.1696476,
+        lng: -0.7776697,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Lomas de Balsares camper park Alicante',
+        direccion: 'Balsares',
+        categoria: 'camping',
+        rating: 4.7,
+        lat: 38.2570244,
+        lng: -0.5588747,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Asociación de Caravanas y Autocaravanas entre Particulares.. (ACAEP)',
+        direccion: 'Partida Daimes, polígono 3-221D, Elx',
+        categoria: 'area_autocaravanas',
+        rating: 4.7,
+        lat: 38.1848491,
+        lng: -0.6789529,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'La Murada, Como Existe.',
+        direccion: 'Lugar Murada, 365B',
+        categoria: 'area_autocaravanas',
+        rating: 4.6,
+        lat: 38.1716227,
+        lng: -0.9459624999999998,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Área Natur Playamuchavista',
+        direccion: 'Carrer de les Àncores, 25, El Campello',
+        categoria: 'camping',
+        rating: 4.6,
+        lat: 38.3922744,
+        lng: -0.4130669,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Sí Encanto!',
+        direccion: 'Partida Carrús, Elx',
+        categoria: 'camping',
+        rating: 4.6,
+        lat: 38.2999881,
+        lng: -0.7292038,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'CAMPING EL REFUGIO',
+        direccion: 'Vía Pista, 28, el Fondó',
+        categoria: 'camping',
+        rating: 4.6,
+        lat: 38.4381898,
+        lng: -0.9176833,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camper Area 7',
+        direccion: 'Av. del Fabraquer, 14, El Campello',
+        categoria: 'area_autocaravanas',
+        rating: 4.6,
+        lat: 38.4047967,
+        lng: -0.4096907,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Las Palmeras Resort',
+        direccion: 'Carrer Gutiérrez de Cárdenas, N-340a, Km 708, Crevillent',
+        categoria: 'camping',
+        rating: 4.6,
+        lat: 38.2408146,
+        lng: -0.8120641,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Zona de Acampada Rincón Bello',
+        direccion: 'Petrer',
+        categoria: 'camping',
+        rating: 4.5,
+        lat: 38.4804855,
+        lng: -0.7003058999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Playa Rebollo',
+        direccion: 'Urb. Costa Bella-la Marina, 18, Guardamar del Segura',
+        categoria: 'area_autocaravanas',
+        rating: 4.5,
+        lat: 38.1282501,
+        lng: -0.6486786999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camping Casa PiNova',
+        direccion: 'Lugar Derramador, 1203, Elx',
+        categoria: 'camping',
+        rating: 4.5,
+        lat: 38.2107631,
+        lng: -0.7045293,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Dolores / Alicante',
+        direccion: 'Diseminado Mateos, 3',
+        categoria: 'area_autocaravanas',
+        rating: 4.5,
+        lat: 38.140314,
+        lng: -0.7496788999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'EL Pinet Playa CAMPERPARK- ALICANTE',
+        direccion: 'Cami del Pinet, 55, la Marina',
+        categoria: 'area_autocaravanas',
+        rating: 4.5,
+        lat: 38.1518169,
+        lng: -0.6420914,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Dunas y Mar - Camperpark',
+        direccion: 'Carretera Alicante-torrev, 1, San Fulgencio',
+        categoria: 'camping',
+        rating: 4.5,
+        lat: 38.1208353,
+        lng: -0.6548257,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camping & Resort La Marina.',
+        direccion: '49H2+WG, Av. de l\'Alegría, s / n, la Marina',
+        categoria: 'camping',
+        rating: 4.3,
+        lat: 38.1302246,
+        lng: -0.6497609999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Las Palmeras Camping Crevillente',
+        direccion: 'N-340a, Km 708, Crevillent',
+        categoria: 'camping',
+        rating: 4.3,
+        lat: 38.2404479,
+        lng: -0.8120394,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camper Área Campello Beach',
+        direccion: 'Carrer de Juan de la Cierva, 8, El Campello',
+        categoria: 'area_autocaravanas',
+        rating: 4.3,
+        lat: 38.3948389,
+        lng: -0.4104182,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camping Bahía Santa Pola',
+        direccion: 'Carretera de Elche a, 11, km 11 (N - 322, Santa Pola',
+        categoria: 'camping',
+        rating: 4.2,
+        lat: 38.2006628,
+        lng: -0.570066,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camper Área Caravaning',
+        direccion: 'La Marina, 2093A, Elche',
+        categoria: 'area_autocaravanas',
+        rating: 4.2,
+        lat: 38.1591463,
+        lng: -0.6375519,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camping El Jardín',
+        direccion: 'Carrer Doctor Severo Ochoa, 39, El Campello',
+        categoria: 'camping',
+        rating: 4.1,
+        lat: 38.3929405,
+        lng: -0.415273,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camping Palm - Mar',
+        direccion: 'CAMPING PALM-MAR, Ctra. Alicante-Cartagena, KM 70',
+        categoria: 'camping',
+        rating: 4.1,
+        lat: 38.0728468,
+        lng: -0.6529195,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Sunset Camper Area',
+        direccion: 'Ctra. Benimagrell, 36, El Campello',
+        categoria: 'camping',
+        rating: 4.1,
+        lat: 38.38892999999999,
+        lng: -0.4149,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camping La Pedrera',
+        direccion: 'C. Cañada de Andrea, 100, Bigastro',
+        categoria: 'camping',
+        rating: 4.1,
+        lat: 38.0512825,
+        lng: -0.8982846999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camping Rincón de Luna Guardamar Park',
+        direccion: 'CV-920, 57, Guardamar del Segura',
+        categoria: 'camping',
+        rating: 3.9,
+        lat: 38.1017484,
+        lng: -0.6905572000000001,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'La Forgaria calle Vereda 80A Almoradi',
+        direccion: 'La Forgaria 80a Road, Almoradí',
+        categoria: 'area_autocaravanas',
+        rating: 3.7,
+        lat: 38.1118698,
+        lng: -0.8151143000000001,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'Camping Santa Pola',
+        direccion: 'Carrer Electricistas, 14, Pol. Ind.',
+        categoria: 'area_autocaravanas',
+        rating: 3.5,
+        lat: 38.2083069,
+        lng: -0.5737966999999999,
+        tipo: 'camping'
+    },
+    {
+        nombre: 'La ponderosa',
+        direccion: '4894+4Q, San Fulgencio',
+        categoria: 'area_autocaravanas',
+        rating: 2.3,
+        lat: 38.1177694,
+        lng: -0.6930864999999999,
+        tipo: 'camping'
+    }
+];
